a63d59d6bb2d3a42b79abbaeae2ca59c843bbba0
Natan Macedo <natan.mac.n@gmail.com>
2025-05-19 14:15:20 -0300

    :sparkles: Adicionado CRUD do projeto

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,9 +3,12 @@ package com.generation.blogpessoal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
+import java.util.List;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 	
+	//SELECT * FROM tb_postagem WHERE titulo = ?
 
 }
