d1d4aa7e72849672dfd84e0cf66f18fc46aba57d
Natan Macedo <natan.mac.n@gmail.com>
2025-05-20 14:34:27 -0300

    :sparkles: Realizado CRUD da classe tema, implementado repository e controller da classe tema + testes do insomnia + tabela tb_temas no BD do projeto

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,37 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "A descrição não pode ser vazia!")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
